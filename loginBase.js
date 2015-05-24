2f6087337fc561888ae04913b3612dd1bc125a06
indianninja <hemilgajjar98@gmail.com>
2015-05-23 23:44:11 -0700

    File Cleanup
    Deleted unnecessary stuff
    Renamed stuff

--- loginBase.js
+++
@@ -1,6 +0,0 @@
-var fb = new Firebase("https://<your-firebase>.firebaseio.com");
-$(document).ready(function(){
-    $("#submit").click(function(){
-        fb.push({ name: document.getElementById('nameBox').value});
-    });
-});
